b415889545a9fd71f46524747cf6b472a29e6572
Harikrishna Prasanth Nair <112437819+itsNairr@users.noreply.github.com>
2026-09-14 11:42:06 -0400

    Add: jump-game-ii - submission-5

---
+++ Data Structures & Algorithms/jump-game-ii/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        memo = {}
+
+        def solve(i):
+            if i in memo:
+                return memo[i]
+            if i == len(nums) - 1:
+                return 0 #cause were at the end
+
+            res = float("inf")
+            end = min(len(nums), i + nums[i] + 1) #+1 and no -1 for inclusive
+            for j in range(i+1, end):
+                res = min(res, 1 + solve(j))
+            memo[i] = res
+            return memo[i]
+        return solve(0)
+
+            
